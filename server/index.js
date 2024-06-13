181eac4a41873d0ce48c4d1fab91b3f221b5666b
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2024-06-13 13:31:36 -0600

    style: :bug: Fix new relic configs
    Fix new relic configs
    
    Fix new relic configs

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
-"use strict";
+// Uncomment after adding New Relic agent to project
+const newrelic = require("newrelic");
 
-const newrelic = require("newrelic"); // Asegúrate de importar New Relic antes de cualquier otra configuración
 const express = require("express");
 const logger = require("pino")();
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+
 const fs = require("fs");
 const open = require("open");
+
 const RestaurantRecord = require("./model").Restaurant;
 const MemoryStorage = require("./storage").Memory;
 
@@ -59,21 +61,24 @@ exports.start = function (PORT, STATIC_DIR, DATA_FILE, TEST_DIR) {
   app.post(API_URL_ORDER, jsonParser, function (req, res, next) {
     logger.info(req.body, "checkout");
 
-    // Añadir atributos personalizados a New Relic
+    /*************************************
+    /*         Custom attributes         *
+    /*************************************
     var order = req.body;
     var itemCount = 0;
     var orderTotal = 0;
-    order.items.forEach(function (item) {
+    order.items.forEach(function(item) { 
       itemCount += item.qty;
       orderTotal += item.price * item.qty;
     });
-
+    
     newrelic.addCustomAttributes({
-      customer: order.deliverTo.name,
-      restaurant: order.restaurant.name,
-      itemCount: itemCount,
-      orderTotal: orderTotal,
+      'customer': order.deliverTo.name,
+      'restaurant': order.restaurant.name,
+      'itemCount': itemCount,
+      'orderTotal': orderTotal
     });
+    /*************************************/
 
     return res.send(201, { orderId: Date.now() });
   });
